04dad8c51287f6eb09414fe017570114911c72c9
ygrek <ygrek@autistici.org>
2008-10-20 17:48:29 +0300

    typename fix

--- include/AD/contain/linklist.h
+++ include/AD/contain/linklist.h
@@ -1,24 +1,24 @@
 //////////////////////////////////////////////////////////////////////////////
 // NOTICE:
 //
-// ADLib, Prop and their related set of tools and documentation are in the 
-// public domain.   The author(s) of this software reserve no copyrights on 
+// ADLib, Prop and their related set of tools and documentation are in the
+// public domain.   The author(s) of this software reserve no copyrights on
 // the source code and any code generated using the tools.  You are encouraged
 // to use ADLib and Prop to develop software, in both academic and commercial
 // settings, and are free to incorporate any part of ADLib and Prop into
 // your programs.
 //
-// Although you are under no obligation to do so, we strongly recommend that 
+// Although you are under no obligation to do so, we strongly recommend that
 // you give away all software developed using our tools.
 //
-// We also ask that credit be given to us when ADLib and/or Prop are used in 
-// your programs, and that this notice be preserved intact in all the source 
+// We also ask that credit be given to us when ADLib and/or Prop are used in
+// your programs, and that this notice be preserved intact in all the source
 // code.
 //
-// This software is still under development and we welcome any suggestions 
+// This software is still under development and we welcome any suggestions
 // and help from the users.
 //
-// Allen Leung 
+// Allen Leung
 // 1994
 //////////////////////////////////////////////////////////////////////////////
 
@@ -40,7 +40,7 @@ template <class T>
 
       struct Node {
          T element; Node * last, * next;
-         Node (const T& e, Node * prev, Node * succ) 
+         Node (const T& e, Node * prev, Node * succ)
             : element(e), last(prev), next(succ) {}
       };
 
@@ -49,15 +49,15 @@ template <class T>
       int size;                         // length of list
 
       LinkedList(const LinkedList &);               // No copy constructor
-      LinkedList& operator = (const LinkedList&);   // No assignment 
+      LinkedList& operator = (const LinkedList&);   // No assignment
 
    public:
       class Unimplemented{};
 
       LinkedList() : front(0), back(0), size(0) {}
-     ~LinkedList(); 
+     ~LinkedList();
 
-      int length() const                { return size; } 
+      int length() const                { return size; }
       Bool is_empty() const             { return size == 0; }
       T& head() const                   { return front->element; }
       T& tail() const                   { return back->element; }
@@ -69,7 +69,7 @@ template <class T>
       LinkedList& delete_before (LinkedListIter<T>& i);
       LinkedList& delete_after  (LinkedListIter<T>& i);
 
-      LinkedList& insert_head (const T& e); 
+      LinkedList& insert_head (const T& e);
       LinkedList& insert_tail (const T& e);
       LinkedList& delete_head ();
       LinkedList& delete_tail ();
@@ -80,17 +80,17 @@ template <class T>
 template <class T>
    class LinkedListIter {
       friend class LinkedList<T>;
-      LinkedList<T>::Node * cursor;
-      LinkedListIter(LinkedList<T>::Node * c) : cursor(c) {}
+      typename LinkedList<T>::Node * cursor;
+      LinkedListIter(typename LinkedList<T>::Node * c) : cursor(c) {}
    public:
       LinkedListIter() : cursor(0) {}
       LinkedListIter(LinkedList<T>& list) : cursor(list.front) {}
      ~LinkedListIter() {}
 
       void operator = (LinkedList<T>& list)   { cursor = list.front; }
-      void operator = (LinkedListIter& i)           { cursor = i.cursor; } 
+      void operator = (LinkedListIter& i)           { cursor = i.cursor; }
 
-      operator LinkedList<T>::Node * () const { return cursor; }
+      operator typename LinkedList<T>::Node * () const { return cursor; }
 
       void operator ++ (int)                  { cursor = cursor->next; }
       void operator ++ ()                     { cursor = cursor->next; }
@@ -114,23 +114,23 @@ template <class T>
    }
 
 template <class T>
-   inline LinkedList<T>& LinkedList<T>::insert_head(const T& e) 
-   {  Node * n = new Node(e,0,front); 
+   inline LinkedList<T>& LinkedList<T>::insert_head(const T& e)
+   {  Node * n = new Node(e,0,front);
       if (front) front->last = n;
       else back = n;
       front = n;
       size++;
-      return *this; 
-   } 
+      return *this;
+   }
 
 template <class T>
    inline LinkedList<T>& LinkedList<T>::insert_tail(const T& e)
-   {  Node * n = new Node(e,back,0); 
+   {  Node * n = new Node(e,back,0);
       if (back) back->next = n;
       else front = n;
       back = n;
       size++;
-      return *this; 
+      return *this;
    }
 
 template <class T>
@@ -162,11 +162,11 @@ template <class T>
       size++;
       return *this;
    }
-   
+
 template <class T>
    LinkedList<T>& LinkedList<T>::insert_after
        (const T& e, LinkedListIter<T>& i)
-   {  Node * n = new Node(e,i.cursor,i.cursor->next); 
+   {  Node * n = new Node(e,i.cursor,i.cursor->next);
       i.cursor->next->last = n;
       i.cursor->next = n;
       size++;
